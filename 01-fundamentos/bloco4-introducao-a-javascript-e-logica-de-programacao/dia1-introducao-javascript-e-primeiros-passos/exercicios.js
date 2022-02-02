80057eed7a95bcecac15d318432a1a8a5368d51c
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-02 20:14:26 -0300

    Adicionado exercicio 3

--- 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript-e-primeiros-passos/exercicios.js
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript-e-primeiros-passos/exercicios.js
@@ -1,5 +1,6 @@
 const a = 2;
 const b = 3;
+const c = 1;
 
 console.log('Soma: ' + (a + b));
 console.log('Subtração: ' + (a - b));
@@ -11,4 +12,12 @@ if (a > b) {
     console.log("'a' é maior que 'b'");
 } else {
     console.log("'a' é menor que 'b'");
+}
+
+if (a > b && a > c) {
+    console.log("'a' é o maior número");
+} else if (b > a && b > c) {
+    console.log("'b' é o maior número");
+} else {
+    console.log("'c' é o maior número");
 }
