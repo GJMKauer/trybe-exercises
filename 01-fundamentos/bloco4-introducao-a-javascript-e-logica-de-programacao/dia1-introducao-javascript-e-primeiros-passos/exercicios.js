cc161b95ffc96e5edb0e7c36ed8a5596477a942d
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-02 20:41:34 -0300

    Adicionado exercicio 8

--- 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript-e-primeiros-passos/exercicios.js
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript-e-primeiros-passos/exercicios.js
@@ -73,20 +73,31 @@
 //         console.log('Erro: não é uma peça de xadrez.');
 // }
 
-let nota = 99;
+// let nota = 99;
 
-if (nota >= 90 && nota <= 100) {
-    console.log('A');
-} else if (nota >= 80  && nota <= 100) {
-    console.log('B');
-} else if (nota >= 70  && nota <= 100) {
-    console.log('C');
-} else if (nota >= 60  && nota <= 100) {
-    console.log('D');
-} else if (nota >= 50  && nota <= 100) {
-    console.log('E');
-} else if (nota >= 0 && nota < 50) {
-    console.log('F');
-} else {
-    console.log('Erro: Nota inválida.');
+// if (nota >= 90 && nota <= 100) {
+//     console.log('A');
+// } else if (nota >= 80  && nota <= 100) {
+//     console.log('B');
+// } else if (nota >= 70  && nota <= 100) {
+//     console.log('C');
+// } else if (nota >= 60  && nota <= 100) {
+//     console.log('D');
+// } else if (nota >= 50  && nota <= 100) {
+//     console.log('E');
+// } else if (nota >= 0 && nota < 50) {
+//     console.log('F');
+// } else {
+//     console.log('Erro: Nota inválida.');
+// }
+
+const numeroA = 2;
+const numeroB = 4;
+const numeroC = 8;
+
+let isImpar = false;
+
+if (numeroA % 2 === 0 && numeroB % 2 === 0 && numeroC % 2 ===0) {
+    isImpar = true;
 }
+console.log(isImpar);
