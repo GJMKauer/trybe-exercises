7c416cc8368e24e20e8b4544e28619fa31209af7
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-02 20:03:13 -0300

    Aula dia 4.1

---
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript-e-primeiros-passos/codigos-aula-course/script2.js
@@ -0,0 +1,105 @@
+// const myName = "Gabriel";
+// const birthCity = "Sapucaia do Sul";
+// let birthYear = 1998;
+// birthYear = 2030;
+// birthCity = "Porto Alegre";
+
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
+
+// -----------------------------------------------
+
+// let patientId = '50';
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof(patientId));
+// console.log(typeof(isEnrolled));
+// console.log(typeof(patientInfo));
+// console.log(typeof(patientEmail));
+// console.log(typeof(patientAge));
+
+// -----
+
+// const base = 5;
+// let height = 8;
+// const area = base * height;
+// const perimeter = (base + height + base + height);
+
+// console.log(area);
+// console.log(perimeter);
+
+// -----------------------------------------------
+
+// const pessoaCandidata = 81;
+
+// if (pessoaCandidata >= 80) {
+//     console.log('Parabéns, você foi aprovada(o)!')
+// } else if (pessoaCandidata >= 60) {
+//     console.log('Você está na nossa lista de espera')
+// } else {
+//     console.log('Você foi reprovada(o)')
+// }
+
+// -----------------------------------------------
+
+// const currentHour = 4;
+// let message = '';
+
+// if (currentHour>= 22) {
+//     message = 'Não deveríamos comer nada, é hora de dormir';
+// } else if (currentHour >= 18) {
+//     message = 'Rango da noite, vamos jantar :D';
+// } else if (currentHour >= 14) {
+//     message = 'Vamos fazer um bolo pro café da tarde?';
+// } else if (currentHour >= 11) {
+//     message = 'Hora do almoço!!!';
+// } else if (currentHour >= 4) {
+//     message = 'Hmmm, cheiro de café recém passado'
+// } else {
+//     message = 'Não deveríamos comer nada, é hora de dormir';
+// }
+
+// console.log(message);
+
+// -----
+
+// let weekDay = "segunda-feira";
+
+// if (
+//   weekDay === "segunda-feira" ||
+//   weekDay === "terça-feira" ||
+//   weekDay === "quarta-feira" ||
+//   weekDay === "quinta-feira" ||
+//   weekDay === "sexta-feira"
+// ) {
+//   console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+// } else {
+//   console.log("FINALMENTE, descanso merecido UwU");
+// }
+
+// -----------------------------------------------
+
+// let pessoaCandidata = 'aprovada';
+
+// switch (pessoaCandidata) {
+//     case 'aprovada':
+//         console.log('Aprovado(a)!');
+//         break;
+//     case 'lista':
+//         console.log('Na lista de espera. Boa sorte! :)');
+//         break;
+//     case 'reprovada':
+//         console.log('Você foi reprovado(a). :(');
+//         break;
+//     default:
+//         console.log('não se aplica');
+// }
+
+// -----------------------------------------------
+
---
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript-e-primeiros-passos/codigos-aula-course/scripts.js
@@ -0,0 +1,94 @@
+// let age = 20;
+// age = 30;
+// let name = "O Incrível Hulk";
+
+// console.log(age);
+// console.log(name);
+
+// -----------------------------
+
+// let movie = "Avengers"; //string literal
+// let score = 10.89; //number literal
+// let isValid = true; //boolean
+// let height; //undefined
+// let color = null; //Redefinição
+
+// -----------------------------
+
+// let salary = 3500;
+
+// console.log(salary + salary);
+// console.log(salary - salary);
+// console.log(salary * salary);
+// console.log(salary / salary);
+// console.log(3 ** 4);
+
+// salary++;
+
+// console.log(salary);
+
+// salary--;
+
+// console.log(salary);
+
+// -----------------------------
+
+// let trybe = 19.40;
+
+// if (trybe >= 14 && trybe < 14.40) {
+//     console.log('Esquenta');
+// } else if (trybe >= 16.20 && trybe < 18.00) {
+//     console.log('Aula ao Vivo')
+// } else if (trybe >= 19.40 && trybe < 20.00) {
+//     console.log('Fechamento')
+// } else {
+//     console.log('Fora dos momentos síncronos')
+// }
+
+// -----------------------------
+
+// const comida = 'pão na chapa';
+// const bebida = 'suco';
+
+// if (bebida === 'cafézinho' && comida === 'pão na chapa') {
+//   console.log('Muito obrigado pela refeição :)');
+// } else {
+//   console.log('Acho que houve um engano com meu pedido');
+// }
+
+// -----
+
+// const bebidaPrincipal = "vodka";
+// const bebidaAlternativa = "whisky";
+
+// if (
+//   bebidaPrincipal === "cafezinho" ||
+//   bebidaAlternativa === "suco de laranja"
+// ) {
+//   console.log("Obrigado por me atender :D");
+// } else {
+//   console.log("Ei, eu não pedi isso!");
+// }
+
+// -----
+
+// console.log((2 + 2) === 4);
+// console.log(!(2+2) === 4);
+
+// -----------------------------
+
+// let trafficLight = 'verde';
+
+// switch (trafficLight) {
+//   case "vermelho":
+//     console.log("pare");
+//     break;
+//   case "amarelo":
+//     console.log("atenção");
+//     break;
+//   case "verde":
+//     console.log("siga");
+//     break;
+//   default:
+//     console.log("valor não identificado");
+// }
