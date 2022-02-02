8a4a2026f030610630e1ae2be379e06911488a97
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-02 20:16:08 -0300

    Adicionado exercicio 4

--- 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript-e-primeiros-passos/exercicios.js
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript-e-primeiros-passos/exercicios.js
@@ -21,3 +21,11 @@ if (a > b && a > c) {
 } else {
     console.log("'c' é o maior número");
 }
+
+if (a > 0) {
+    console.log('positive');
+} else if (a < 0) {
+    console.log('negative');
+} else {
+    console.log('zero');
+}
