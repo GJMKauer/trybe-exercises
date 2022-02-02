2e95eed90da8b0e7d862e59847a1d5227a2f8704
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-02 20:10:35 -0300

    Adicionado exercicio 1

---
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript-e-primeiros-passos/exercicios.js
@@ -0,0 +1,8 @@
+const a = 2;
+const b = 3;
+
+console.log('Soma: ' + (a + b));
+console.log('Subtração: ' + (a - b));
+console.log('Multiplicação: ' + (a * b));
+console.log('Divisão: ' + (a / b));
+console.log('Módulo: ' + (a % b));
