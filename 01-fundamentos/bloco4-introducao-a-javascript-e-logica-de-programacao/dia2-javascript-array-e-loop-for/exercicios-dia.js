54d6dd0556b170836f762f12b5428bde180d14cf
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-03 20:23:59 -0300

    Adicionado exercicios bonus 1 e 2.

--- 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-javascript-array-e-loop-for/exercicios-dia.js
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-javascript-array-e-loop-for/exercicios-dia.js
@@ -106,5 +106,33 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // -----
-// Exercício bônus
+// Exercício bônus - 1
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//       if (numbers[index] < numbers[secondIndex]) {
+//         let position = numbers[index];
+//         numbers[index] = numbers[secondIndex];
+//         numbers[secondIndex] = position;
+//       }
+//     }
+//   }
+// console.log(numbers);
+
+// -----
+// Exercício bônus - 2
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//       if (numbers[index] > numbers[secondIndex]) {
+//         let position = numbers[index];
+//         numbers[index] = numbers[secondIndex];
+//         numbers[secondIndex] = position;
+//       }
+//     }
+//   }
+// console.log(numbers);
+
+// -----
+// Exercício bônus - 3
 
