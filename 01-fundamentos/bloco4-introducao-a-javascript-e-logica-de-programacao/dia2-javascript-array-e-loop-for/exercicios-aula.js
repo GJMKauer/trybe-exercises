32ae7b984929ee17a4f1f9070780aa82a90c5880
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-03 18:23:47 -0300

    Adicionado os codigos dos exercicios de fixacao do conteudo do course

---
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-javascript-array-e-loop-for/exercicios-aula.js
@@ -0,0 +1,40 @@
+// Exercício 1 (Array)
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// -----
+// Exercício 2 (Array)
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+// -----
+// Exercicio 3 (Array)
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('Contato');
+
+// console.log(menu);
+
+// ---------------------------
+// Exercicio 1 (For)
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (let index = 0; index < groceryList.length; index += 1) {
+    // console.log(groceryList[index]);
+// }
+
+// ---------------------------
+// Exercicio 1 (For/of)
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+// for (let numeros of names) {
+//     console.log(numeros);
+// }
