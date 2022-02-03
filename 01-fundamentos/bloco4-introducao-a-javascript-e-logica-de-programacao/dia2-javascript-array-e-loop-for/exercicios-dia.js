b5da53e13adde65961f2cf17a3cb53d4b8466a24
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-03 18:49:05 -0300

    Adicionado exercicio 5.

--- 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-javascript-array-e-loop-for/exercicios-dia.js
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-javascript-array-e-loop-for/exercicios-dia.js
@@ -30,23 +30,31 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // -----
 // Exercício 4
 
-let soma = 0;
-for (let index = 0; index < numbers.length; index += 1) {
-    soma += numbers[index];
-}
-let mediaAritmetica = soma / numbers.length;
-console.log(mediaAritmetica);
+// let soma = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//     soma += numbers[index];
+// }
+// let mediaAritmetica = soma / numbers.length;
+// console.log(mediaAritmetica);
 
-if (mediaAritmetica >= 20) {
-    console.log('Valor maior que 20.')
-} else {
-    console.log('Valor menor que 20.')
-}
+// if (mediaAritmetica >= 20) {
+//     console.log('Valor maior que 20.')
+// } else {
+//     console.log('Valor menor que 20.')
+// }
 
 // -----
 // Exercício 5
 
+// let maior = 0;
 
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] > maior) {
+//         maior = numbers[index];
+//     }
+// }
+
+// console.log(maior);
 
 // -----
 // Exercício 6
