0aa172a68a2c506fa9a01b2a9af2b926f00193b7
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-03 18:34:40 -0300

    Adicionado exercicio 3.

--- 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-javascript-array-e-loop-for/exercicios-dia.js
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-javascript-array-e-loop-for/exercicios-dia.js
@@ -11,16 +11,21 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // -----
 // Exercício 2
 
-let soma = 0;
-for (let index = 0; index < numbers.length; index += 1) {
-    soma += numbers[index];
-}
-console.log(soma);
+// let soma = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//     soma += numbers[index];
+// }
+// console.log(soma);
 
 // -----
 // Exercício 3
 
-
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index];
+}
+let mediaAritmetica = soma / numbers.length;
+console.log(mediaAritmetica);
 
 // -----
 // Exercício 4
