158bd39c28b67275302e2bc944c1cfece5912f65
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-07 18:40:56 -0300

    Adicionado conteudo de funçoes

--- 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-javascript-objetos-e-funcoes/aula-course/script.js
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-javascript-objetos-e-funcoes/aula-course/script.js
@@ -167,3 +167,124 @@
 //   console.log(value);
 // };
 //resultado: hamburguer, bife, acarajé;
+
+
+// Funções
+// Funções de um carro
+
+// let statusCarro = 'desligado';
+// let aceleracao = 0;
+// let rotacaoDoVolante = 0;
+
+// function ligarDesligar() {
+//     if (statusCarro === 'desligado') {
+//         statusCarro = 'ligado';
+//     } else {
+//         statusCarro = 'desligado';
+//     }
+//     return statusCarro;
+// }
+
+// function acelerar(incremento) {
+//     aceleracao += incremento;
+
+//     return 'Acelerando a ' + aceleracao + 'm/s²';
+// }
+
+// function frear(decremento) {
+//     aceleracao -= decremento;
+
+//     return 'Desacelerando para ' + aceleracao + 'm/s²';
+// }
+
+// function girarVolante(anguloRotacao) {
+//     rotacaoDoVolante = anguloRotacao;
+
+//     return rotacaoDoVolante + 'º';
+// }
+
+// ligarDesligar();
+// acelerar(20);
+// girarVolante(-45);
+// frear(5);
+// girarVolante(0);
+// frear(15);
+// ligarDesligar();
+
+
+// -----
+// Função de Bom Dia
+
+// Sem função
+// let nome = 'João';
+
+// console.log('Bom dia, ' + nome);
+
+
+// // Com função
+// function bomDiaTryber(nome) {
+//   console.log('Bom dia, ' + nome);
+// }
+
+// bomDiaTryber('João'); // Bom dia, João
+// bomDiaTryber('Julia'); // Bom dia, Julia
+// bomDiaTryber('Marcelo'); // Bom dia, Marcelo
+
+
+// -----
+// Funções sem parâmetro
+
+// function bomDia() {
+//     return 'Bom dia!';
+//   }
+  
+//   console.log(bomDia()); // Bom dia!
+
+// -----
+// let status = 'deslogado';
+
+// function logarDeslogar() {
+//   if (status === 'deslogado') {
+//     status = 'logado';
+//   } else {
+//     status = 'deslogado';
+//   }
+// }
+
+// console.log(status); // deslogado
+
+// logarDeslogar();
+// console.log(status); // logado
+
+// logarDeslogar();
+// console.log(status); // deslogado
+
+// console.log('O usuário está ' + status); // O usuário está deslogado
+
+
+// -----
+// Funções com parâmetro
+
+// Com função
+// function soma(a, b) {
+//     return a + b;
+//   }
+  
+//   console.log(soma(5, 2)); // 7
+
+//   function maiorNum(primeiroNum, segundoNum) {
+//     if (primeiroNum > segundoNum) {
+//       return primeiroNum + ' é maior que ' + segundoNum;
+//     } else if (segundoNum > primeiroNum) {
+//       return segundoNum + ' é maior que ' + primeiroNum;
+//     } else {
+//       return 'Os números são iguais';
+//     }
+//   }
+  
+//   console.log(maiorNum(10, 20)); // 20 é maior que 10
+//   console.log(maiorNum(2, -5)); // 2 é maior que -5
+//   console.log(maiorNum(1, 1)); // Os números são iguais
+
+
+  // -----
