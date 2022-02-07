fd5055a1bf139f71e15e6eac66b450f7c66b4b37
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-07 20:06:51 -0300

    Adicionado exercicio 2 de Objetos e For/In.

--- 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-javascript-objetos-e-funcoes/exercicios/exercicios-dia.js
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-javascript-objetos-e-funcoes/exercicios/exercicios-dia.js
@@ -2,17 +2,18 @@
 
 // Exercício 1
 
-// let info = {
-//     personagem: 'Margarida',
-//     origem: 'Pato Donald',
-//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-//   };
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
 // console.log('Bem-vinda, ' + info.personagem);
 
 
 // Exercício 2
 
-
+// info['recorrente'] = 'Sim';
+// console.log(info);
 
 
 // Exercício 3
