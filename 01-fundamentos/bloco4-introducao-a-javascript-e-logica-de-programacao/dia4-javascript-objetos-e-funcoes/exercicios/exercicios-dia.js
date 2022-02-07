a6849b6bd5ae0e757c2f4b686aebac1aefccfb6f
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-07 19:53:30 -0300

    Adicionado exercicio 1 de Objetos e For/In.

---
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-javascript-objetos-e-funcoes/exercicios/exercicios-dia.js
@@ -0,0 +1,108 @@
+// Objetos e For/In
+
+// Exercício 1
+
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+// console.log('Bem-vinda, ' + info.personagem);
+
+
+// Exercício 2
+
+
+
+
+// Exercício 3
+
+
+
+
+// Exercício 4
+
+
+
+
+// Exercício 5
+
+
+
+
+// Exercício 6
+
+
+
+
+// Exercício 7
+
+
+
+
+// Exercício 8
+
+
+
+
+// ------------------------------
+// Funções
+
+// Exercício 1
+
+
+
+
+// Exercício 2
+
+
+
+
+// Exercício 3
+
+
+
+
+// Exercício 4
+
+
+
+
+// Exercício 5
+
+
+
+
+// Exercício 6
+
+
+
+
+// Exercício 7
+
+
+
+
+// ------------------------------
+// Exercício Bônus 1
+
+
+
+
+// Exercício Bônus 2
+
+
+
+
+// Exercício Bônus 3
+
+
+
+
+// Exercício Bônus 4
+
+
+
+
+// Exercício Bônus 5
+
