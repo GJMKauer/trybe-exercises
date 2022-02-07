8498f8ecf0003bf83c41e8e2822e14cf1a26d16b
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-07 15:46:46 -0300

    Iniciado os exercicios para fixar

---
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-javascript-objetos-e-funcoes/aula-course/exercicios.js
@@ -0,0 +1,36 @@
+// ----- Objetos - Para Fixar 1
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3,
+    }
+}
+
+
+// ----- Objetos - Para Fixar 2
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+
+
+// ----- Objetos - Para Fixar 3
+
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
+
+
+// ----- Objetos - Para Fixar 4
+
+console.log(player.bestInTheWorld);
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
+
+// ----- Objetos - Para Fixar 5
+
+console.log(player.medals);
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+
+
+// ------------------------------
+
