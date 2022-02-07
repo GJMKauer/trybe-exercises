555639d275be3f0b3ccee6e486997044841b2308
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-07 20:26:19 -0300

    Adicionado exercicio 5 de Objetos e For/In.

--- 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-javascript-objetos-e-funcoes/exercicios/exercicios-dia.js
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-javascript-objetos-e-funcoes/exercicios/exercicios-dia.js
@@ -32,8 +32,21 @@ info['recorrente'] = 'Sim';
 
 // Exercício 5
 
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim',
+//   };
 
-
+//   if (info.recorrente === 'Sim' && info2.recorrente === 'Sim') {
+//   for (let key in info) {
+//       if (info[key] === 'Sim' && info2[key]) {
+//           console.log('Ambos recorrentes');
+//       } else 
+//     console.log (info[key], 'e', info2[key]);
+//   }
+// }
 
 // Exercício 6
 
