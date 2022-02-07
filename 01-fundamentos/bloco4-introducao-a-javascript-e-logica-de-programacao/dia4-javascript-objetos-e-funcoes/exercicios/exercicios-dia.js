e58a72a09a9ef162e48ec5f77d083b0f98a8fd5c
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-07 20:34:54 -0300

    Adicionado exercicio 6 de Objetos e For/In.

--- 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-javascript-objetos-e-funcoes/exercicios/exercicios-dia.js
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-javascript-objetos-e-funcoes/exercicios/exercicios-dia.js
@@ -2,17 +2,17 @@
 
 // Exercício 1
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
 // console.log('Bem-vinda, ' + info.personagem);
 
 
 // Exercício 2
 
-info['recorrente'] = 'Sim';
+// info['recorrente'] = 'Sim';
 // console.log(info);
 
 
@@ -46,10 +46,22 @@ info['recorrente'] = 'Sim';
 //     console.log (info[key], 'e', info2[key]);
 //   }
 
+
 // Exercício 6
 
-
-
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+// console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0]['titulo'] + '"');
 
 // Exercício 7
 
