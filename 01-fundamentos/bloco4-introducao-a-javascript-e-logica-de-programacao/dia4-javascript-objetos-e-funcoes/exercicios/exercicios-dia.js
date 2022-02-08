e05591b1a431de169be61668934f88bf5c27d2e1
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-07 21:08:52 -0300

    Adicionado exercicio 2 de Funcoes.

--- 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-javascript-objetos-e-funcoes/exercicios/exercicios-dia.js
+++ 01-fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-javascript-objetos-e-funcoes/exercicios/exercicios-dia.js
@@ -100,8 +100,16 @@
 
 // Exercício 2
 
-
-
+// function indiceMaiorValor(numeros) {
+//     let indiceMaior = 0;
+//     for (index = 0; index < numeros.length; index += 1) {
+//         if (numeros[indiceMaior] < numeros[index]) {
+//             indiceMaior = index;
+//         }
+//     }
+//     return indiceMaior;
+// }
+// console.log(indiceMaiorValor([2, 3, 6, 7, 10, 1]));
 
 // Exercício 3
 
