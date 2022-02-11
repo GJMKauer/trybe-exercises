cf8325dbaa2d4192c86c65cbd6f04d960d367004
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-11 18:38:00 -0300

    Adiciona exercicio 5 do course.

--- 01-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/dia3-javascript-eventos/main.js
+++ 01-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/dia3-javascript-eventos/main.js
@@ -46,15 +46,22 @@ input.addEventListener("input", alteraTexto);
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
-let redirecionamento = document.getElementById("my-spotrybefy");
 function redireciona() {
-  console.log(redirecionamento);
     window.location.href = 'https://gjmkauer.github.io/';
 }
-redirecionamento.addEventListener('dblclick', redireciona);
+myWebpage.addEventListener('dblclick', redireciona);
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+function alteraCor() {
+    myWebpage.style.color = 'green';
+}
+myWebpage.addEventListener('mouseover', alteraCor);
+
+function voltaCor() {
+    myWebpage.style.color = 'white';
+}
+myWebpage.addEventListener('mouseout', voltaCor);
 
 // Segue abaixo um exemplo do uso de event.target:
 
