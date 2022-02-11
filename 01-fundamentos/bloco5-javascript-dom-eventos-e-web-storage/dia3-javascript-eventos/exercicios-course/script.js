4aadd6ed579c65597172f7f2ec2d895f18d3356e
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-11 19:21:26 -0300

    Adicionado exercicio 1.

--- 01-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/dia3-javascript-eventos/exercicios-course/script.js
+++ 01-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/dia3-javascript-eventos/exercicios-course/script.js
@@ -22,3 +22,36 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+const dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+function criaDias() {
+  let calendarioLista = document.querySelector("#days");
+  for (let i = 0; i < dezDaysList.length; i += 1) {
+    let calendarioItems = document.createElement("li");
+    calendarioItems.innerText = dezDaysList[i];
+    calendarioItems.className = "day";
+
+    calendarioLista.appendChild(calendarioItems);
+  }
+}
+criaDias();
+
+function criaFeriado() {
+  for (i = 25; i <= 26; i += 1) {
+    document.getElementsByClassName("day")[i].className += " holiday";
+  }
+  for (i = 32; i <= 32; i += 1) {
+    document.getElementsByClassName("day")[i].className += " holiday";
+  }
+}
+criaFeriado();
+
+function criaSexta() {
+  for (i = 5; i <= 26; i += 7) {
+    document.getElementsByClassName("day")[i].className += " friday";
+  }
+}
+criaSexta();
+
