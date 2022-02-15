b8d9b5aa9a21042884077c53cef3b6ecef663710
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-15 00:38:34 -0300

    Alterado completamente o exercicio. Finalizado. Fiz conforme o modelo do Joao Pster.

--- 01-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/dia4-javascript-web-storage/exercicios-aula/script.js
+++ 01-fundamentos/bloco5-javascript-dom-eventos-e-web-storage/dia4-javascript-web-storage/exercicios-aula/script.js
@@ -1,19 +1,62 @@
 let texto = document.querySelector("#texto");
 texto.style.textAlign = "justify";
 
+let inputCorDeFundo = document.querySelector("#inputCorDeFundo");
+let inputCorDoTexto = document.querySelector("#inputCorDoTexto");
+let inputTamanhoFonte = document.querySelector("#inputTamanhoFonte");
+let inputAlturaLinha = document.querySelector("#inputAlturaLinha");
+let seletorFonte = document.querySelector("#selecaoDeFonte");
+
 function definirPadraoInicial() {
-    localStorage.setItem('backgroundColor', )
+  localStorage.setItem("backgroundColor", "#ffffff");
+  localStorage.setItem("textColor", "#000000)");
+  localStorage.setItem("fontSize", "16");
+  localStorage.setItem("lineHeight", "18");
+  localStorage.setItem("fontFamily", "Times");
+}
+function colocarLocalStorageNoInput() {
+  inputCorDeFundo.value = localStorage.getItem("backgroundColor");
+  inputCorDoTexto.value = localStorage.getItem("textColor");
+  inputTamanhoFonte.value = localStorage.getItem("fontSize");
+  inputAlturaLinha.value = localStorage.getItem("lineHeight");
+  seletorFonte.value = localStorage.getItem("fontFamily");
+}
+function valoresIniciais() {
+  texto.style.backgroundColor = localStorage.getItem("backgroundColor");
+  texto.style.color = localStorage.getItem("textColor");
+  texto.style.fontSize = localStorage.getItem("fontSize") + "px";
+  texto.style.lineHeight = localStorage.getItem("lineHeight") + "px";
+  texto.style.fontFamily = localStorage.getItem("fontFamily");
+}
+if (localStorage.length <= 0) {
+  definirPadraoInicial();
 }
 
-function mudaCorFundo() {
-  let corFundo = document.getElementById("selecionaCorBackground").value;
-  texto.style.backgroundColor = corFundo;
-  document.getElementById("corBackground").value = corFundo;
-  localStorage.setItem("backgroundColor", corFundo);
-}
-function mudaCorTexto() {
-  let corTexto = document.getElementById("selecionaCorTexto").value;
-  texto.style.color = corTexto;
-  document.getElementById("corTexto").value = corTexto;
-  localStorage.setItem("fontColor", corTexto);
-}
+inputCorDeFundo.addEventListener("blur", function () {
+  texto.style.backgroundColor = inputCorDeFundo.value;
+  localStorage.setItem("backgroundColor", inputCorDeFundo.value);
+});
+
+inputCorDoTexto.addEventListener("blur", function () {
+  texto.style.color = inputCorDoTexto.value;
+  localStorage.setItem("textColor", inputCorDoTexto.value);
+});
+inputTamanhoFonte.addEventListener("blur", function () {
+  texto.style.fontSize = inputTamanhoFonte.value + "px";
+  localStorage.setItem("fontSize", inputTamanhoFonte.value);
+});
+
+inputAlturaLinha.addEventListener("blur", function () {
+  texto.style.lineHeight = inputAlturaLinha.value + "px";
+  localStorage.setItem("lineHeight", inputAlturaLinha.value);
+});
+
+seletorFonte.addEventListener("click", function () {
+  texto.style.fontFamily = selecaoDeFonte.value;
+  localStorage.setItem("fontFamily", selecaoDeFonte.value);
+});
+
+window.onload = function () {
+  colocarLocalStorageNoInput();
+  valoresIniciais();
+};
