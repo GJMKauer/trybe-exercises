ee2c5aff15098ec8aca6f9eb5302f63d8a2eb7ec
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-18 17:36:38 -0300

    Terminado o conteudo do course. Bora pros exercicios!

--- 01-fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/dia1-html-e-css-forms/para-fixar-parte-iv/script.js
+++ 01-fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/dia1-html-e-css-forms/para-fixar-parte-iv/script.js
@@ -2,3 +2,20 @@
 const INPUT_TEXT = document.querySelector("#input-text");
 const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 const HREF_LINK = document.querySelector("#href");
+
+function previneClickLink(event) {
+  event.preventDefault();
+}
+HREF_LINK.addEventListener("click", previneClickLink);
+
+function previneClickBox(event) {
+  event.key.preventDefault();
+}
+INPUT_CHECKBOX.addEventListener("click", previneClickBox);
+
+function previneTexto(event) {
+  if (event.key != "a") {
+    event.preventDefault();
+  }
+}
+INPUT_TEXT.addEventListener("keypress", previneTexto);
