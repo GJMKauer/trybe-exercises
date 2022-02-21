1f59642facefbe83fc88a7415daf7d0b90ff3adf
Gabriel Kauer <gjmkauer@hotmail.com>
2022-02-21 20:26:18 -0300

    Finalizado exercicio 2.

--- 01-fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/dia2-bibliotecas-javascript-e-frameworks-css/exercicios-aula/script.js
+++ 01-fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/dia2-bibliotecas-javascript-e-frameworks-css/exercicios-aula/script.js
@@ -1,14 +1,37 @@
-const botaoEnviar = document.querySelector("#botao-enviar");
 const botaoApagar = document.querySelector("#botao-apagar");
-const formulario = document.querySelector("#formulario");
-
-function previneEnviar(event) {
-  event.preventDefault();
-}
-botaoEnviar.addEventListener("click", previneEnviar);
+const formulario = document.querySelector("#form");
 
 function apagar() {
   formulario.reset();
 }
 botaoApagar.addEventListener("click", apagar);
 
+const validation = new JustValidate("#form");
+
+validation
+  .addField("#nomeCandidato", [
+    {
+      rule: "minLength",
+      value: 10,
+      errorMessage: "Este campo precisa conter no mínimo 10 caracteres.",
+    },
+    {
+      rule: "maxLength",
+      value: 50,
+      errorMessage: "Este campo pode conter até 50 caracteres.",
+    },
+    {
+      rule: "required",
+      errorMessage: "Necessário informar seu nome.",
+    },
+  ])
+  .addField("#emailCandidato", [
+    {
+      rule: "required",
+      errorMessage: "Necessário informar seu e-mail.",
+    },
+    {
+      rule: "email",
+      errorMessage: "E-mail inválido!",
+    },
+  ]);
