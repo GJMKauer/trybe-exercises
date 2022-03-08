6aa4976263bba6a59d386fc54b85b7ed1fc15d90
Gabriel Kauer <gjmkauer@hotmail.com>
2022-03-07 22:41:33 -0300

    Adiciona exercicio 3.

--- 01-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia3-primeiros-passos-em-jest/exercicios-aula/sum.js
+++ 01-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia3-primeiros-passos-em-jest/exercicios-aula/sum.js
@@ -7,14 +7,32 @@
 
 // module.exports = sum;
 
-function myRemove(arr, item) {
-  let newArr = [];
-  for (let index = 0; index < arr.length; index += 1) {
-    if (item !== arr[index]) {
-      newArr.push(arr[index]);
-    }
-  }
-  return newArr;
-}
+// const myRemove = (arr, item) => {
+//   let newArr = [];
+//   for (let index = 0; index < arr.length; index += 1) {
+//     if (item !== arr[index]) {
+//       newArr.push(arr[index]);
+//     }
+//   }
+//   return newArr;
+// }
 
-module.exports = myRemove;
+// module.exports = myRemove;
+
+const myFizzBuzz = (num) => {
+  if (typeof num !== 'number') {
+    return false;
+  }
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz';
+  }
+  if (num % 3 === 0) {
+    return 'fizz';
+  }
+  if (num % 5 === 0) {
+    return 'buzz';
+  }
+  return num;
+};
+
+module.exports = myFizzBuzz;
--- 01-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia3-primeiros-passos-em-jest/exercicios-aula/sum.test.js
+++ 01-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia3-primeiros-passos-em-jest/exercicios-aula/sum.test.js
@@ -15,17 +15,37 @@
 //   });
 // });
 
-const myRemove = require('./sum');
+// const myRemove = require('./sum');
 
-describe('Exercício 2', () => {
-  const array = [1, 2, 3, 4];
-  it('Quando o array for [1, 2, 3, 4] e item for 3, retorna o array [1, 2, 4]', () => {
-    expect(myRemove(array, 3)).toEqual([1, 2, 4]);
+// describe('Exercício 2', () => {
+//   const array = [1, 2, 3, 4];
+//   it('Quando o array for [1, 2, 3, 4] e item for 3, retorna o array [1, 2, 4]', () => {
+//     expect(myRemove(array, 3)).toEqual([1, 2, 4]);
+//   });
+//   it('Quando o array for [1, 2, 3, 4] e item for 3, não retornar o array [1, 2, 3, 4]', () => {
+//     expect(myRemove(array, 3)).not.toEqual(array);
+//   });
+//   it('Quando o array for [1, 2, 3, 4] e item for 5, retorna o array [1, 2, 3, 4]', () => {
+//     expect(myRemove(array, 5)).toEqual(array);
+//   });
+// });
+
+const myFizzBuzz = require('./sum');
+
+describe('Exercício 3', () => {
+  it('A função retorna "fizzbuzz" quando o número é 15', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
   });
-  it('Quando o array for [1, 2, 3, 4] e item for 3, não retornar o array [1, 2, 3, 4]', () => {
-    expect(myRemove(array, 3)).not.toEqual(array);
+  it('A função retorna "fizz" quando o número é 9', () => {
+    expect(myFizzBuzz(9)).toBe('fizz');
   });
-  it('Quando o array for [1, 2, 3, 4] e item for 5, retorna o array [1, 2, 3, 4]', () => {
-    expect(myRemove(array, 5)).toEqual(array);
+  it('A função retorna "buzz" quando o número é 10', () => {
+    expect(myFizzBuzz(10)).toBe('buzz');
+  });
+  it('A função retorna 4 quando o número é 4', () => {
+    expect(myFizzBuzz(4)).toBe(4);
+  });
+  it('A função retorna false quando o valor não é um número', () => {
+    expect(myFizzBuzz('q')).toBeFalsy();
   });
 });
