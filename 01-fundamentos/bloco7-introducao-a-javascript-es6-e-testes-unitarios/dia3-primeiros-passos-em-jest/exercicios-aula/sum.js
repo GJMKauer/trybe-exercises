8a76478302a5b8c3231700b073275da3527625b6
Gabriel Kauer <gjmkauer@hotmail.com>
2022-03-07 22:31:43 -0300

    Adiciona exercicio 2.

--- 01-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia3-primeiros-passos-em-jest/exercicios-aula/sum.js
+++ 01-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia3-primeiros-passos-em-jest/exercicios-aula/sum.js
@@ -1,8 +1,20 @@
-const sum = (a, b) => {
-  if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new Error('parameters must be numbers');
-  }
-  return a + b;
-};
+// const sum = (a, b) => {
+//   if (typeof a !== 'number' || typeof b !== 'number') {
+//     throw new Error('parameters must be numbers');
+//   }
+//   return a + b;
+// };
 
-module.exports = sum;
+// module.exports = sum;
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+module.exports = myRemove;
--- 01-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia3-primeiros-passos-em-jest/exercicios-aula/sum.test.js
+++ 01-fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia3-primeiros-passos-em-jest/exercicios-aula/sum.test.js
@@ -1,16 +1,31 @@
-const sum = require('./sum');
+// const sum = require('./sum');
 
-describe('Exercício 1', () => {
-  it('A soma dos valores 4 e 5 é 9', () => {
-    expect(sum(4, 5)).toBe(9);
+// describe('Exercício 1', () => {
+//   it('A soma dos valores 4 e 5 é 9', () => {
+//     expect(sum(4, 5)).toBe(9);
+//   });
+//   it('A soma dos valores 0 e 0 é 0', () => {
+//     expect(sum(0, 0)).toBe(0);
+//   });
+//   it('A função lança um erro quando os parâmetros são 4 e "5"', () => {
+//     expect(() => sum(4, '5')).toThrow();
+//   });
+//   it('A função lança a mensagem de erro "parameters must be numbers" quando os parâmetros são 4 e "5"', () => {
+//     expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
+//   });
+// });
+
+const myRemove = require('./sum');
+
+describe('Exercício 2', () => {
+  const array = [1, 2, 3, 4];
+  it('Quando o array for [1, 2, 3, 4] e item for 3, retorna o array [1, 2, 4]', () => {
+    expect(myRemove(array, 3)).toEqual([1, 2, 4]);
   });
-  it('A soma dos valores 0 e 0 é 0', () => {
-    expect(sum(0, 0)).toBe(0);
+  it('Quando o array for [1, 2, 3, 4] e item for 3, não retornar o array [1, 2, 3, 4]', () => {
+    expect(myRemove(array, 3)).not.toEqual(array);
   });
-  it('A função lança um erro quando os parâmetros são 4 e "5"', () => {
-    expect(() => sum(4, '5')).toThrow();
-  });
-  it('A função lança a mensagem de erro "parameters must be numbers" quando os parâmetros são 4 e "5"', () => {
-    expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
+  it('Quando o array for [1, 2, 3, 4] e item for 5, retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemove(array, 5)).toEqual(array);
   });
 });
