c53a35ad4435defe062e99f083ea9a3a0040b044
Gabriel Kauer <gjmkauer@hotmail.com>
2022-03-10 19:10:05 -0300

    Adiciona para fixar de Array.find.

--- 01-fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/conteudo-course/script.js
+++ 01-fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/conteudo-course/script.js
@@ -83,18 +83,51 @@
 
 // -----
 
-// Para Fixar - forEach
-// 1 - Use o método forEach chamando a callback showEmailList para apresentar os emails
-const emailListInData = [
-  'roberta@email.com',
-  'paulo@email.com',
-  'anaroberta@email.com',
-  'fabiano@email.com',
-];
+// // Para Fixar - Array.forEach
+// // 1 - Use o método forEach chamando a callback showEmailList para apresentar os emails
+// const emailListInData = [
+//   'roberta@email.com',
+//   'paulo@email.com',
+//   'anaroberta@email.com',
+//   'fabiano@email.com',
+// ];
 
-const showEmailList = (email) => {
-  console.log(`O email ${email} está cadastrado em nosso banco de dados!`);
-};
+// const showEmailList = (email) => {
+//   console.log(`O email ${email} está cadastrado em nosso banco de dados!`);
+// };
 
-// Adicione seu código aqui
-emailListInData.forEach(showEmailList);
+// // Adicione seu código aqui
+// emailListInData.forEach(showEmailList);
+
+// // Para Fixar - Array.find
+// // 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// const findDivisibleBy3And5 = (number) => number % 3 === 0 && number % 5 === 0;
+
+// const verify3And5 = numbers.find(findDivisibleBy3And5);
+
+// console.log(verify3And5);
+
+// // 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+// const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// const findNameWithFiveLetters = (name) => name.length === 5;
+
+// const verifyIs5 = names.find(findNameWithFiveLetters);
+
+// console.log(verifyIs5);
+
+// // 3 - Utilize o find para encontrar a música com id igual a 31031685 , caso ela exista:
+// const musicas = [
+//   { id: '31031685', title: 'Partita in C moll BWV 997' },
+//   { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+//   { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+// ];
+
+// const findMusic = (id) => {
+//   return musicas.find((musica) => musica.id === id);
+// }
+
+// console.log(findMusic('31031685'));
+
