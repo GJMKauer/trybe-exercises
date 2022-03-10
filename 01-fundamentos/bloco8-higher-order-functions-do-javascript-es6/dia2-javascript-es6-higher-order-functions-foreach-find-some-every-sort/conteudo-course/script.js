6e59ade832f540dcb8f8fc921c5b37670f3f8283
Gabriel Kauer <gjmkauer@hotmail.com>
2022-03-10 17:32:33 -0300

    Adiciona conteudo inicial do course e um desafio pratico da aula ao vivo.

---
+++ 01-fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/conteudo-course/script.js
@@ -0,0 +1,83 @@
+// Diferença da utilização de loop for para forEach
+
+// // Com loop for:
+// const students = [
+//   { name: 'Maria', grade: 70, approved: '' },
+//   { name: 'José', grade: 56, approved: '' },
+//   { name: 'Roberto', grade: 90, approved: '' },
+//   { name: 'Ana', grade: 81, approved: '' },
+// ];
+
+// function verifyGrades() {
+//   for (let i = 0; i < students.length; i += 1) {
+//     const student = students[i];
+//     if (student.grade >= 60) {
+//       student.approved = 'Aprovado';
+//     } else {
+//       student.approved = 'Recuperação';
+//     }
+//   }
+// }
+
+// verifyGrades();
+
+// console.log(students);
+// // [
+// //   { name: 'Maria', grade: 70, approved: 'Aprovado' },
+// //   { name: 'José', grade: 56, approved: 'Recuperação' },
+// //   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+// //   { name: 'Ana', grade: 81, approved: 'Aprovado' }
+// // ]
+
+// // --
+// // Com forEach:
+// const students = [
+//   { name: 'Maria', grade: 70, approved: '' },
+//   { name: 'José', grade: 56, approved: '' },
+//   { name: 'Roberto', grade: 90, approved: '' },
+//   { name: 'Ana', grade: 81, approved: '' },
+// ];
+
+// function verifyGrades() {
+//   students.forEach((student, index) => {
+//     if (student.grade >= 60) {
+//       students[index].approved = 'Aprovado';
+//     } else {
+//       students[index].approved = 'Recuperação';
+//     }
+//   });
+// }
+
+// verifyGrades();
+
+// console.log(students);
+// // [
+// //   { name: 'Maria', grade: 70, approved: 'Aprovado' },
+// //   { name: 'José', grade: 56, approved: 'Recuperação' },
+// //   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+// //   { name: 'Ana', grade: 81, approved: 'Aprovado' }
+// // ]
+
+// Diferença de utilização de condicional if/else para Array e Array.find
+
+// // Com if/else:
+// const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+// let firstMultipleOf5;
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 5 === 0) {
+//     firstMultipleOf5 = numbers[index];
+//     break;
+//   }
+// }
+
+// console.log(firstMultipleOf5);
+// // 50
+
+// // --
+// // Com Array.find:
+// const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+// const firstMultipleOf5 = numbers.find((number) => number % 5 === 0);
+
+// console.log(firstMultipleOf5);
+// // 50
+
