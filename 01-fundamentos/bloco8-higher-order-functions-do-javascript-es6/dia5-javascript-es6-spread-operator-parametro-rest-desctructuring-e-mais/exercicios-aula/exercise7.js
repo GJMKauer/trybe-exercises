b2941c472d181b0a5c1b0bd05293f916b5ce1dca
Gabriel Kauer <gjmkauer@hotmail.com>
2022-03-15 18:57:35 -0300

    Adiciona exercicio 7.

--- 01-fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia5-javascript-es6-spread-operator-parametro-rest-desctructuring-e-mais/exercicios-aula/exercise7.js
+++ 01-fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia5-javascript-es6-spread-operator-parametro-rest-desctructuring-e-mais/exercicios-aula/exercise7.js
@@ -20,7 +20,10 @@ const ships = [
 ];
 
 // escreva shipLength abaixo
-
-console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
-console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
-console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
+const shipLength = ({ name, length, measurementUnit }) => `${name} is ${length} ${measurementUnit} long.`;
+console.log(shipLength(ships[0]));
+console.log(shipLength(ships[1]));
+console.log(shipLength(ships[2]));
+// console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
+// console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
+// console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
