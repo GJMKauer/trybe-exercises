a33c993e85425afdbdbab88400628d1a58fc4f0e
Gabriel Kauer <gjmkauer@hotmail.com>
2022-03-15 15:33:02 -0300

    Adiciona conteudo de Array Destructuring.

---
+++ 01-fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia5-javascript-es6-spread-operator-parametro-rest-desctructuring-e-mais/conteudo-course/array-destructuring.js
@@ -0,0 +1,35 @@
+// // Para Fixar 1:
+// // 1 - Produza o mesmo resultado do código, porém utilizando o array destructuring para recolher a função e a saudação.
+// const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+// saudacoes[1](saudacoes[0]); // Olá
+
+// // Produza o mesmo resultado acima, porém utilizando array destructuring
+// const [firstElement] = saudacoes;
+// console.log(firstElement); // Olá.
+
+// // --
+
+// // Para Fixar 2:
+// // 2 - A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis.
+// let comida = 'gato';
+// let animal = 'água';
+// let bebida = 'arroz';
+
+// console.log(comida, animal, bebida); // arroz gato água
+
+// // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+// const [food, pet, drink] = [bebida, comida, animal]
+// console.log(food, pet, drink);
+
+// // --
+
+// Para Fixar 3:
+// 3 - O array abaixo possui alguns números que não condizem com o conteúdo que ele deveria possuir. Através de array destructuring, faça com que existam apenas números pares na variável numerosPares
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+const [, , , ...evenNumbers] = numerosPares; // ???
+console.log(evenNumbers);
