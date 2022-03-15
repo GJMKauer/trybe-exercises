0f7ca868b84cfd7fa2bc1ad2ee9a10a263fbf88b
Gabriel Kauer <gjmkauer@hotmail.com>
2022-03-15 19:06:10 -0300

    Adiciona exercicio 9.

--- 01-fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia5-javascript-es6-spread-operator-parametro-rest-desctructuring-e-mais/exercicios-aula/exercise9.js
+++ 01-fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia5-javascript-es6-spread-operator-parametro-rest-desctructuring-e-mais/exercicios-aula/exercise9.js
@@ -8,3 +8,7 @@ const yearSeasons = {
   autumn: ['September', 'October', 'November'],
   winter: ['December', 'January', 'February'],
 };
+const { spring, summer, autumn, winter } = yearSeasons;
+
+const yearMonths = [...spring, ...summer, ...autumn, ...winter];
+console.log(yearMonths);
