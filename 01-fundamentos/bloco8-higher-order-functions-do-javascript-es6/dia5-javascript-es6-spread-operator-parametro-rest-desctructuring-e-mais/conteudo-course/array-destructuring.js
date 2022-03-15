6abfe0fdbd255e1298505ce6556894445d93fc42
Gabriel Kauer <gjmkauer@hotmail.com>
2022-03-15 14:45:14 -0300

    Adiciona conteudo de spread operator.

---
+++ 01-fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia5-javascript-es6-spread-operator-parametro-rest-desctructuring-e-mais/conteudo-course/array-destructuring.js
@@ -0,0 +1,13 @@
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['Mamão', 'Maçã', 'Morango'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItems = ['Granola', 'Neston', 'Leite Ninho'];
+
+const fruitSalad = (fruit, additional) => {
+  // Escreva sua função aqui
+  const total = [...specialFruit, ...additionalItems];
+  return total;
+};
+
+console.log(fruitSalad(specialFruit, additionalItems));
