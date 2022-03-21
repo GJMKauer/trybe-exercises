e18a934128e1001047b8caf88dec8931b755fac7
Gabriel Kauer <gjmkauer@hotmail.com>
2022-03-21 19:01:38 -0300

    Adiciona o Jest nos exercicios.

--- 01-fundamentos/bloco9-javacript-e-testes-assincronos/dia1-javascript-assincrono-e-callbacks/exercicios-aula/script.js
+++ 01-fundamentos/bloco9-javacript-e-testes-assincronos/dia1-javascript-assincrono-e-callbacks/exercicios-aula/script.js
@@ -129,48 +129,48 @@
 
 // // --
 
-// Exercício 6: Por fim, o robô Curiosity tem uma taxa de sucesso de envio de mensagem de 60% devido ao fato de o robô já estar muito ocupado com outras operações. Logo, adicione na função sendMarsTemperature uma outra callback que contenha as ações a serem tomadas em caso de falha.
-const messageDelay = () => Math.floor(Math.random() * 5000);
+// // Exercício 6: Por fim, o robô Curiosity tem uma taxa de sucesso de envio de mensagem de 60% devido ao fato de o robô já estar muito ocupado com outras operações. Logo, adicione na função sendMarsTemperature uma outra callback que contenha as ações a serem tomadas em caso de falha.
+// const messageDelay = () => Math.floor(Math.random() * 5000);
 
-const getMarsTemperature = () => {
-  const maxTemperature = 58;
-  return Math.floor(Math.random() * maxTemperature);
-};
+// const getMarsTemperature = () => {
+//   const maxTemperature = 58;
+//   return Math.floor(Math.random() * maxTemperature);
+// };
 
-const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9) / 5 + 32;
+// const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9) / 5 + 32;
 
-const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+// const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
 
-const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+// const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
-const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
+// const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
 
-// definição da função sendMarsTemperature...
-const sendMarsTemperature = (onSuccess, onError) => {
-  const messageSuccessfullySent = Math.random() <= 0.6;
-  setTimeout(() => {
-    if (messageSuccessfullySent) {
-      return onSuccess(getMarsTemperature());
-    } else {
-      return onError('Robot is busy');
-    }
-  }, messageDelay());
-};
+// // definição da função sendMarsTemperature...
+// const sendMarsTemperature = (onSuccess, onError) => {
+//   const messageSuccessfullySent = Math.random() <= 0.6;
+//   setTimeout(() => {
+//     if (messageSuccessfullySent) {
+//       return onSuccess(getMarsTemperature());
+//     } else {
+//       return onError('Robot is busy');
+//     }
+//   }, messageDelay());
+// };
 
-// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
-sendMarsTemperature(temperatureInFahrenheit, handleError);
+// // imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+// sendMarsTemperature(temperatureInFahrenheit, handleError);
 
-// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
-sendMarsTemperature(greet, handleError);
+// // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+// sendMarsTemperature(greet, handleError);
 
 // // --
 
-// // Exercício 7: Escreva um teste que verifique a chamada da callback de uma função uppercase , que transforma as letras de uma palavra em letras maiúsculas. Lembre-se de ter cuidado com os falso-positivos em testes assíncronos.
-// const uppercase = (str, callback) => {
-//   setTimeout(() => {
-//     callback(str.toUpperCase());
-//   }, 500);
-// };
+// Exercício 7: Escreva um teste que verifique a chamada da callback de uma função uppercase , que transforma as letras de uma palavra em letras maiúsculas. Lembre-se de ter cuidado com os falso-positivos em testes assíncronos.
+const uppercase = (str, callback) => {
+  setTimeout(() => {
+    callback(str.toUpperCase());
+  }, 500);
+};
 
 // // --
 
