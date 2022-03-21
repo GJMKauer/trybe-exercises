e2a4b89fe1375929842e1f889fb320ca91529ddc
Gabriel Kauer <gjmkauer@hotmail.com>
2022-03-21 19:48:04 -0300

    Adiciona exercicio 10 do dia.

--- 01-fundamentos/bloco9-javacript-e-testes-assincronos/dia1-javascript-assincrono-e-callbacks/exercicios-aula/script.js
+++ 01-fundamentos/bloco9-javacript-e-testes-assincronos/dia1-javascript-assincrono-e-callbacks/exercicios-aula/script.js
@@ -176,70 +176,59 @@
 
 // // --
 
-// Exercício 8: Para o próximo exercício, você vai sentir na pele o primeiro dia de um treinador Pokémon! No laboratório do Professor Carvalho, você é informado de que existem três pokémons disponíveis: Bulbasaur, Charmander e Squirtle. Complete a chamada da função getPokemonDetails de modo que ela imprima no console os detalhes do pokémon que você escolheu. PS: é possível que o sistema do Professor Carvalho apresente erros caso o pokémon não exista no banco de dados, então não se esqueça de tratá-los também, combinado?
-const pokemons = [
-  {
-    name: 'Bulbasaur',
-    type: 'Grass',
-    ability: 'Razor Leaf',
-  },
-  {
-    name: 'Charmander',
-    type: 'Fire',
-    ability: 'Ember',
-  },
-  {
-    name: 'Squirtle',
-    type: 'Water',
-    ability: 'Water Gun',
-  },
-];
+// // Exercício 8: Para o próximo exercício, você vai sentir na pele o primeiro dia de um treinador Pokémon! No laboratório do Professor Carvalho, você é informado de que existem três pokémons disponíveis: Bulbasaur, Charmander e Squirtle. Complete a chamada da função getPokemonDetails de modo que ela imprima no console os detalhes do pokémon que você escolheu. PS: é possível que o sistema do Professor Carvalho apresente erros caso o pokémon não exista no banco de dados, então não se esqueça de tratá-los também, combinado?
+// const pokemons = [
+//   {
+//     name: 'Bulbasaur',
+//     type: 'Grass',
+//     ability: 'Razor Leaf',
+//   },
+//   {
+//     name: 'Charmander',
+//     type: 'Fire',
+//     ability: 'Ember',
+//   },
+//   {
+//     name: 'Squirtle',
+//     type: 'Water',
+//     ability: 'Water Gun',
+//   },
+// ];
 
-function getPokemonDetails(filter, callback) {
-  setTimeout(() => {
-    if (pokemons.find(filter) === undefined) {
-      return callback(new Error('Não temos esse pokémon para você :('), null);
-    }
-    const pokemon = pokemons.find(filter);
+// function getPokemonDetails(filter, callback) {
+//   setTimeout(() => {
+//     if (pokemons.find(filter) === undefined) {
+//       return callback(new Error('Não temos esse pokémon para você :('), null);
+//     }
+//     const pokemon = pokemons.find(filter);
 
-    const { name, type, ability } = pokemon;
+//     const { name, type, ability } = pokemon;
 
-    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}.`;
+//     const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}.`;
 
-    callback(null, messageFromProfOak);
-  }, 2000);
-}
+//     callback(null, messageFromProfOak);
+//   }, 2000);
+// }
 
-getPokemonDetails(
-  (pokemon) => pokemon.name === 'Charmander',
-  (error, message) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(message);
-    }
-  }
-);
+// getPokemonDetails(
+//   (pokemon) => pokemon.name === 'Charmander',
+//   (error, message) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log(message);
+//     }
+//   }
+// );
 
-module.exports = {
-  getPokemonDetails,
-};
+// module.exports = {
+//   getPokemonDetails,
+// };
 
-// --
+// // --
 
-// Exercício 9: Está no arquivo script.test.js.
+// // Exercício 9: Está no arquivo script.test.js.
 
-// --
+// // --
 
-// // Exercício 10: Para este exercício, tente adivinhar a saída dos console.log dos testes abaixo sem executá-los, e veja se compreendeu bem o funcionamento do beforeEach e do afterEach.
-// beforeEach(() => console.log('1 - beforeEach'));
-// afterEach(() => console.log('1 - afterEach'));
-
-// test('', () => console.log('1 - test'));
-
-// describe('Scoped / Nested block', () => {
-//   beforeEach(() => console.log('2 - beforeEach'));
-//   afterEach(() => console.log('2 - afterEach'));
-
-//   test('', () => console.log('2 - test'));
-// });
+// // Exercício 10: Está no arquivo script.test.js.
