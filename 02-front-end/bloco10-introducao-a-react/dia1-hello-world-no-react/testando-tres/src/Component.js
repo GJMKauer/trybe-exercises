9845732201a51a0cf9457046f776375d4c2928f3
Gabriel Kauer <gjmkauer@hotmail.com>
2022-04-04 16:15:07 -0300

    Adiciona conteúdo de React (Course) do dia 10.1.

---
+++ 02-front-end/bloco10-introducao-a-react/dia1-hello-world-no-react/testando-tres/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Gabriel</h1>
+        <p>Aluno de Desenvolvimento Web pela Trybe.</p>
+      </div>
+    );
+  }
+}
+
+export default Component;
