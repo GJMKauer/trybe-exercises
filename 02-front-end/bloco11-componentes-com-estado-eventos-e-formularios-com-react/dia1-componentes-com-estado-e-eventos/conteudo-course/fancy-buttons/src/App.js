ccaeb75d7c83fb113bbe1eabf796b4a2a53bccaa
Gabriel Kauer <gjmkauer@hotmail.com>
2022-04-07 16:03:02 -0300

    Adiciona mais exercicios de fixação do conteudo do course.

--- 02-front-end/bloco11-componentes-com-estado-eventos-e-formularios-com-react/dia1-componentes-com-estado-e-eventos/conteudo-course/fancy-buttons/src/App.js
+++ 02-front-end/bloco11-componentes-com-estado-eventos-e-formularios-com-react/dia1-componentes-com-estado-e-eventos/conteudo-course/fancy-buttons/src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const handleClick1 = () => {
-  console.log('Botão 1: Oie, você clicou em mim. >.<');
-};
-
-const handleClick2 = () => {
-  console.log('Botão 2: Cuidado comigo! Sou muito frágil rs.');
-};
-
-const handleClick3 = () => {
-  console.log('Botão 3: Yamette kudasai, onii-chan');
-};
-
 class App extends Component {
+  constructor() {
+    super();
+    this.handleClick1 = this.handleClick1.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
+  }
+
+  handleClick1() {
+    console.log('Botão 1: Oie, você clicou em mim. >.<');
+  }
+
+  handleClick2() {
+    console.log('Botão 2: Cuidado comigo! Sou muito frágil rs.');
+  }
+
+  handleClick3() {
+    console.log('Botão 3: Yamette kudasai, onii-chan');
+  }
+
   render() {
     return (
       <div>
-        <button onClick={handleClick1}>Meu botão 1</button>
-        <button onClick={handleClick2}>Meu botão 2</button>
-        <button onClick={handleClick3}>Meu botão 3</button>
+        <button onClick={this.handleClick1}>Meu botão 1</button>
+        <button onClick={this.handleClick2}>Meu botão 2</button>
+        <button onClick={this.handleClick3}>Meu botão 3</button>
         <p>Clique em cada botão para visualizar uma mensagem diferente.</p>
       </div>
     );
