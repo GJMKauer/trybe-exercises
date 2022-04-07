86f70a84fd257f77ca4c2ae7f1bcdabd90de795d
Gabriel Kauer <gjmkauer@hotmail.com>
2022-04-07 18:26:54 -0300

    Adiciona o conteúdo final de React do course do dia 11.1.

--- 02-front-end/bloco11-componentes-com-estado-eventos-e-formularios-com-react/dia1-componentes-com-estado-e-eventos/conteudo-course/fancy-buttons/src/App.js
+++ 02-front-end/bloco11-componentes-com-estado-eventos-e-formularios-com-react/dia1-componentes-com-estado-e-eventos/conteudo-course/fancy-buttons/src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      clickNumber1: 0,
+      clickNumber2: 0,
+      clickNumber3: 0,
+    };
     this.handleClick1 = this.handleClick1.bind(this);
     this.handleClick2 = this.handleClick2.bind(this);
     this.handleClick3 = this.handleClick3.bind(this);
@@ -11,22 +16,31 @@ class App extends Component {
 
   handleClick1() {
     console.log('Botão 1: Oie, você clicou em mim. >.<');
+    this.setState((oldState, _props) => ({
+      clickNumber1: oldState.clickNumber1 + 1,
+    }));
   }
 
   handleClick2() {
     console.log('Botão 2: Cuidado comigo! Sou muito frágil rs.');
+    this.setState((oldState, _props) => ({
+      clickNumber2: oldState.clickNumber2 + 1,
+    }));
   }
 
   handleClick3() {
     console.log('Botão 3: Yamette kudasai, onii-chan');
+    this.setState((oldState, _props) => ({
+      clickNumber3: oldState.clickNumber3 + 1,
+    }));
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleClick1}>Meu botão 1</button>
-        <button onClick={this.handleClick2}>Meu botão 2</button>
-        <button onClick={this.handleClick3}>Meu botão 3</button>
+        <button onClick={this.handleClick1}>{this.state.clickNumber1}</button>
+        <button onClick={this.handleClick2}>{this.state.clickNumber2}</button>
+        <button onClick={this.handleClick3}>{this.state.clickNumber3}</button>
         <p>Clique em cada botão para visualizar uma mensagem diferente.</p>
       </div>
     );
