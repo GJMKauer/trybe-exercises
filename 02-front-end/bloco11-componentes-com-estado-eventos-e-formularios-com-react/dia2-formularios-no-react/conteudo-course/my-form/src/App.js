930be47b33661252ccc4367ce0d8c9643b5a6cca
Gabriel Kauer <gjmkauer@hotmail.com>
2022-04-08 15:15:31 -0300

    Adiciona início do conteúdo do Course.

---
+++ 02-front-end/bloco11-componentes-com-estado-eventos-e-formularios-com-react/dia2-formularios-no-react/conteudo-course/my-form/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Form extends Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      estadoFavorito: '',
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      estadoFavorito: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          Estados e React - Tecnologia fantástica ou reagindo a regionalismos?
+        </h1>
+        <form className="form">
+          <label>
+            Diga qual o seu Estado favorito! De React ou do Brasil, você decide!
+            =)
+            <br></br>
+            <textarea
+              name="estadoFavorito"
+              value={this.state.estadoFavorito}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <input type="number" name="idade" />
+          <br></br>
+          <input type="checkbox" name="vaiComparecer" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
