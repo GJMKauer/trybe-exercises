1185a7b50151fa133f1ab7cbe6fae5f03c79e5ba
Gabriel Kauer <gjmkauer@hotmail.com>
2022-04-13 19:32:19 -0300

    Adiciona mais conteúdo do Course.

--- 02-front-end/bloco12-ciclo-de-vida-de-componentes-e-react-router/dia1-ciclo-de-vida-de-componentes/conteudo-course/jokes/src/App.js
+++ 02-front-end/bloco12-ciclo-de-vida-de-componentes-e-react-router/dia1-ciclo-de-vida-de-componentes/conteudo-course/jokes/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import DadJoke from './components/DadJoke';
+import Counter from './components/Counter';
 
 function App() {
   return (
+    <div>
     <DadJoke />
+    <Counter />
+    </div>
   );
 }
 
