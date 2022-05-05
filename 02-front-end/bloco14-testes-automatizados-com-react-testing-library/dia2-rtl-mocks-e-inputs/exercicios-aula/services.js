c2b99a827846fafbe590db5b6215622b593ff313
Gabriel Kauer <gjmkauer@hotmail.com>
2022-05-05 20:25:08 -0300

    Adiciona exercício 4 da parte 1.

--- 02-front-end/bloco14-testes-automatizados-com-react-testing-library/dia2-rtl-mocks-e-inputs/exercicios-aula/services.js
+++ 02-front-end/bloco14-testes-automatizados-com-react-testing-library/dia2-rtl-mocks-e-inputs/exercicios-aula/services.js
@@ -1,3 +1,12 @@
 const randomNumber = () => Math.floor(Math.random() * 100);
 
-module.exports = randomNumber;
+const returnStrings = (string) => string.toUpperCase();
+// console.log(returnStrings('Abacaxi'));
+
+const returnStrings2 = (string) => string.charAt(0)
+// console.log(returnStrings2('Abacaxi'));
+
+const returnStrings3 = (string1, string2) => string1 + string2;
+// console.log(returnStrings3('Abacaxi', 'Uva'))
+
+module.exports = { randomNumber, returnStrings, returnStrings2, returnStrings3 };
--- 02-front-end/bloco14-testes-automatizados-com-react-testing-library/dia2-rtl-mocks-e-inputs/exercicios-aula/services.test.js
+++ 02-front-end/bloco14-testes-automatizados-com-react-testing-library/dia2-rtl-mocks-e-inputs/exercicios-aula/services.test.js
@@ -1,4 +1,4 @@
-let randomNumber = require('./services');
+let { randomNumber, returnStrings, returnStrings2, returnStrings3 } = require('./services');
 
 afterEach(() => {
   jest.clearAllMocks();
@@ -65,17 +65,83 @@ describe('Exercício 3 - Com a mesma função do Exercício 1, utilize o mock e 
   })
 })
 
-// describe('Exercício 4 - Crie três funções. A primeira deve receber uma string e retorná-la em caixa alta. A segunda recebe uma string e retorna apenas a primeira letra. A terceira recebe duas strings e concatena elas', () => {
-//   it('', () => {
+describe('Exercício 4 - Crie três funções. A primeira deve receber uma string e retorná-la em caixa alta. A segunda recebe uma string e retorna apenas a primeira letra. A terceira recebe duas strings e concatena elas', () => {
+  it('Testa a primeira função', () => {
+    returnStrings = jest.fn().mockReturnValue('ABACAXI');
 
-//   })
-// })
+    returnStrings('Abacaxi');
 
-// describe('Exercício 4.1 - Faça o mock das funções para os seguintes casos: A primeira função agora deve retornar a string em caixa baixa. A segunda função deve retornar a última letra da string. A terceira função deve receber três strings e concatenar elas. Restaure a implementação da PRIMEIRA função e teste se ela foi restaurada', () => {
-//   it('', () => {
+    expect(returnStrings).toHaveBeenCalled();
+    expect(returnStrings).toHaveBeenCalledTimes(1);
+    expect(returnStrings('Abacaxi')).toBe('ABACAXI')
+  })
+  it('Testa a segunda função', () => {
+    returnStrings2 = jest.fn().mockReturnValue('A');
 
-//   })
-// })
+    returnStrings2('Abacaxi');
+
+    expect(returnStrings2).toHaveBeenCalled();
+    expect(returnStrings2).toHaveBeenCalledTimes(1);
+    expect(returnStrings2('Abacaxi')).toBe('A');
+
+  })
+  it('Testa a terceira função', () => {
+    returnStrings3 = jest.fn().mockReturnValue('AbacaxiUva');
+
+    returnStrings3('Abacaxi', 'Uva');
+
+    expect(returnStrings3).toHaveBeenCalled();
+    expect(returnStrings3).toHaveBeenCalledTimes(1);
+    expect(returnStrings3('Abacaxi', 'Uva')).toBe('AbacaxiUva')
+  })
+  it('Mocka a primeira função e testa a nova funcionalidade', () => {
+    returnStrings.mockImplementation((string) => string.toLowerCase());
+
+    returnStrings('ABaCaxi');
+
+    expect(returnStrings).toHaveBeenCalled();
+    expect(returnStrings).toHaveBeenCalledTimes(1);
+    expect(returnStrings('ABaCaxi')).toBe('abacaxi');
+    expect(returnStrings('UVA')).toBe('uva');
+    expect(returnStrings('Eu te amo demais')).toBe('eu te amo demais');
+    expect(returnStrings('goTrybe')).toBe('gotrybe');
+  })
+  it('Mocka a segunda função e testa a nova funcionalidade', () => {
+    returnStrings2.mockImplementation((string) => string.charAt(string.length - 1));
+
+    returnStrings2('Abacaxi');
+
+    expect(returnStrings2).toHaveBeenCalled();
+    expect(returnStrings2).toHaveBeenCalledTimes(1);
+    expect(returnStrings2('ABaCaxi')).toBe('i');
+    expect(returnStrings2('UVA')).toBe('A');
+    expect(returnStrings2('Eu te amo demais')).toBe('s');
+    expect(returnStrings2('goTrybe')).toBe('e');
+  })
+  it('Mocka a terceira função e testa a nova funcionalidade. Restaura a primeira função e testa ela', () => {
+    returnStrings3.mockImplementation((string1, string2, string3) => string1 + string2 + string3);
+
+    returnStrings3('Abacaxi', 'Uva', 'Banana');
+
+    expect(returnStrings3).toHaveBeenCalled();
+    expect(returnStrings3).toHaveBeenCalledTimes(1);
+    expect(returnStrings3('Abacaxi', 'Uva', 'Banana')).toBe('AbacaxiUvaBanana');
+    expect(returnStrings3('Gabriel', 'loves', 'Jackson')).toBe('GabriellovesJackson');
+    expect(returnStrings3('Anna', 'hates', 'Bacon')).toBe('AnnahatesBacon');
+    expect(returnStrings3('I', 'love', 'LoL')).toBe('IloveLoL');
+
+    returnStrings.mockImplementation((string) => string.toUpperCase());
+
+    returnStrings('Abacaxi');
+
+    expect(returnStrings).toHaveBeenCalled();
+    expect(returnStrings).toHaveBeenCalledTimes(1);
+    expect(returnStrings('Abacaxi')).toBe('ABACAXI');
+    expect(returnStrings('Gabriel')).toBe('GABRIEL');
+    expect(returnStrings('Anna')).toBe('ANNA');
+    expect(returnStrings('League of Legends')).toBe('LEAGUE OF LEGENDS');
+  })
+})
 
 // describe('Exercício 5 - Crie uma função que faça requisição para a API dog pictures (https://dog.ceo/dog-api/). Mocke a requisição e crie dois testes. O primeiro deve interpretar que a requisição se resolveu e teve como valor "request success". A segunda deve interpretar que a requisição falhou e ter como valor "request failed"', () => {
 //   it('', () => {
